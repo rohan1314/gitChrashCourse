6da47fcffcf9bf3f469b181198406707d631052a
Rohan Maiti <maitirohan2019@gmail.com>
2024-06-04 02:16:04 +0530

    age function added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 /*
 git init --> to initilise the git version control system to the current directory .. means git will now track the folder
+initially all files will be untracked to make them track we use add command
+git add <file path>   or git add . to add all the files at a time
+git diff --> to see the differance in files
 */
 let name = "Rohan Maiti"
 let age = 21;
